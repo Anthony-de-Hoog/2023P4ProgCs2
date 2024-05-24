38b7edf7d81720e8f2088d8881f8755096cdb760
Anthony-de-Hoog <37657@ma-web.nl>
2024-05-24 12:47:36 +0200

    Opdracht 7 gemaakt

---
+++ EnumOpdracht/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace EnumOpdracht
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Colors myMainThemeColor = Colors.Blue;
+
+            if (myMainThemeColor == Colors.Blue) 
+            { 
+                Console.WriteLine("Dat is mijn kleur"); 
+            }
+        }
+    }
+}
