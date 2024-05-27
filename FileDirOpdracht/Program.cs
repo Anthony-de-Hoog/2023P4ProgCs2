015c8a7f0c5638ab91d8bf4ea909acff707f9fbf
Anthony-de-Hoog <37657@ma-web.nl>
2024-05-27 12:47:28 +0200

    Opdracht 9 gemaakt

---
+++ FileDirOpdracht/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace FileDirOpdracht
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            DirectoryInfo dir = new DirectoryInfo(@"C:\school-backup\ma\bewijzenmap\p1.4\2023P4ProgCs2\2023P4ProgCs2.csproj"); 
+            foreach (FileInfo file in dir.GetFiles())
+            {
+                Console.WriteLine(file.FullName);
+            }
+            foreach (DirectoryInfo file in dir.GetDirectories()) 
+            {
+                Console.WriteLine(dir.FullName);
+            }
+        }
+    }
+}
